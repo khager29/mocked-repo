50ffceb8e02547329059779b9276321fb4fa3d0d
Kimberly Hager <77754003+khager29@users.noreply.github.com>
2023-07-25 09:05:31 -0400

    Update main.js
    First main was too ambitious

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import * as folder1 from "./src/folder1";
-import * as folder2 from "./src/folder2";
-import * as folder3 from "./src/folder3";
-import * as folder4 from "./src/sandbox/folder4";
+import * as folder2 from "./src/sandbox/folder2";
 import * as src from "./src";
 
-export const testUtils = { ...src, folder1, folder2, folder3, folder4 };
+export const testUtils = { ...src, folder1, folder2 };
 
 window.testUtils = testUtils;
