6d4bb4aaee0e586e10aec8dc814379b6ea94c131
Kimberly Hager <77754003+khager29@users.noreply.github.com>
2023-07-25 09:08:19 -0400

    Update function6.js
    Added dependency

--- src/sandbox/folder2/function6.js
+++ src/sandbox/folder2/function6.js
@@ -1,3 +1,6 @@
+import function5 from "./function5.js";
+
 export default function () {
   console.log(`This is the the second function in folder2 in sandbox`);
+  console.log("function5's result was: ", function5());
 }
